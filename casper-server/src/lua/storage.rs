596e64eceaf6add582db3f728d6b248f20797507
Alex Orlenko <aorlenko@yelp.com>
2022-10-27 22:12:35 +0100

    Update Lua Storage integration: - Return (res, err) in case of storage errors - Add `get_responses` function - Add basic tests

--- casper-server/src/lua/storage.rs
+++ casper-server/src/lua/storage.rs
@@ -5,8 +5,8 @@ use std::{borrow::Cow, ops::Deref};
 
 use hyper::Body;
 use mlua::{
-    AnyUserData, ExternalResult, FromLua, Lua, Result as LuaResult, String as LuaString, Table,
-    UserData, UserDataMethods, Value,
+    AnyUserData, FromLua, Lua, Result as LuaResult, String as LuaString, Table, UserData,
+    UserDataMethods, Value,
 };
 use ripemd::{Digest, Ripemd160};
 
@@ -44,20 +44,58 @@ where
         methods.add_async_function(
             "get_response",
             |lua, (this, key): (AnyUserData, Value)| async move {
-                let start = Instant::now();
                 let this = this.borrow::<Self>()?;
+                let start = Instant::now();
 
                 let key = calculate_primary_key(lua, key)?;
-                let resp = this.get_response(key).await.to_lua_err()?;
+                let resp = this.get_response(key).await.map_err(Into::into);
 
                 storage_counter_add!(1, "name" => this.name(), "operation" => "get");
                 storage_histogram_rec!(start, "name" => this.name(), "operation" => "get");
 
-                Ok(resp.map(|resp| {
+                let resp = lua_try!(resp);
+
+                Ok(Ok(resp.map(|resp| {
                     let mut resp = LuaResponse::from(resp);
                     resp.is_stored = true;
                     resp
-                }))
+                })))
+            },
+        );
+
+        methods.add_async_function(
+            "get_responses",
+            |lua, (this, keys): (AnyUserData, Table)| async move {
+                let this = this.borrow::<Self>()?;
+                let start = Instant::now();
+
+                let keys = keys
+                    .raw_sequence_values::<Value>()
+                    .map(|key| key.and_then(|k| calculate_primary_key(lua, k)))
+                    .collect::<LuaResult<Vec<_>>>()?;
+                let items_count = keys.len() as u64;
+                let results = this.get_responses(keys).await;
+
+                storage_counter_add!(items_count, "name" => this.name(), "operation" => "get");
+                storage_histogram_rec!(start, "name" => this.name(), "operation" => "get");
+
+                // Convert results to a table of: { Response | string | boolean }
+                // In case of error we return string
+                // If response is not found then `false`
+                results
+                    .into_iter()
+                    .map(|res| match res {
+                        Ok(Some(resp)) => {
+                            let mut resp = LuaResponse::from(resp);
+                            resp.is_stored = true;
+                            lua.create_userdata(resp).map(Value::UserData)
+                        }
+                        Ok(None) => Ok(Value::Boolean(false)),
+                        Err(err) => lua
+                            .create_string(&err.into().to_string())
+                            .map(Value::String),
+                    })
+                    .collect::<LuaResult<Vec<_>>>()
             },
         );
 
@@ -68,18 +106,17 @@ where
         methods.add_async_function(
             "delete_response",
             |lua, (this, key): (AnyUserData, Value)| async move {
-                let start = Instant::now();
                 let this = this.borrow::<Self>()?;
-                let key = calculate_primary_key(lua, key)?;
+                let start = Instant::now();
 
-                this.delete_responses(ItemKey::Primary(key))
-                    .await
-                    .to_lua_err()?;
+                let key = calculate_primary_key(lua, key)?;
+                let result = this.delete_responses(ItemKey::Primary(key)).await;
 
                 storage_counter_add!(1, "name" => this.name(), "operation" => "delete");
                 storage_histogram_rec!(start, "name" => this.name(), "operation" => "delete");
 
-                Ok(())
+                lua_try!(result.map_err(Into::into));
+                Ok(Ok(Value::Nil))
             },
         );
 
@@ -87,8 +124,8 @@ where
         methods.add_async_function(
             "delete_responses",
             |lua, (this, keys): (AnyUserData, Table)| async move {
-                let start = Instant::now();
                 let this = this.borrow::<Self>()?;
+                let start = Instant::now();
 
                 let primary_keys: Option<Vec<Value>> = keys.raw_get("primary_keys")?;
                 let surrogate_keys: Option<Vec<LuaString>> = keys.raw_get("surrogate_keys")?;
@@ -112,14 +149,14 @@ where
 
                 let items_count: u64 = item_keys.len() as u64;
                 let results = this.delete_responses_multi(item_keys).await;
-                for r in results {
-                    r.to_lua_err()?;
-                }
 
                 storage_counter_add!(items_count, "name" => this.name(), "operation" => "delete");
                 storage_histogram_rec!(start, "name" => this.name(), "operation" => "delete");
 
-                Ok(())
+                for r in results {
+                    lua_try!(r.map_err(Into::into));
+                }
+                Ok(Ok(Value::Nil))
             },
         );
 
@@ -130,8 +167,8 @@ where
         methods.add_async_function(
             "store_response",
             |lua, (this, item): (AnyUserData, Table)| async move {
-                let start = Instant::now();
                 let this = this.borrow::<Self>()?;
+                let start = Instant::now();
 
                 let key: Value = item.raw_get("key")?;
                 let resp: AnyUserData = item.raw_get("response")?;
@@ -141,12 +178,7 @@ where
                 let mut resp = resp.borrow_mut::<LuaResponse>()?;
 
                 // Read Response body (it's consumed and saved)
-                let body = resp
-                    .body_mut()
-                    .buffer()
-                    .await
-                    .to_lua_err()?
-                    .unwrap_or_default();
+                let body = lua_try!(resp.body_mut().buffer().await).unwrap_or_default();
 
                 // Remove hop by hop headers
                 filter_hop_headers(resp.headers_mut());
@@ -167,13 +199,13 @@ where
                         surrogate_keys,
                         ttl: Duration::from_secs_f32(ttl),
                     })
-                    .await
-                    .to_lua_err();
+                    .await;
 
                 storage_counter_add!(1, "name" => this.name(), "operation" => "store");
                 storage_histogram_rec!(start, "name" => this.name(), "operation" => "store");
 
-                result
+                lua_try!(result.map_err(Into::into));
+                Ok(Ok(Value::Nil))
             },
         );
     }
@@ -195,4 +227,69 @@ fn calculate_primary_key(lua: &Lua, key: Value) -> LuaResult<Key> {
         }
     }
     Ok(hasher.finalize().to_vec().into())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+
+    use mlua::{chunk, Lua, Result};
+
+    use super::*;
+    use crate::storage::Backend;
+
+    #[tokio::test]
+    async fn test_storage() -> Result<()> {
+        let lua = Rc::new(Lua::new());
+        lua.set_app_data(Rc::downgrade(&lua));
+
+        let backend_config = serde_yaml::from_str(
+            r#"
+            backend: memory
+            max_size: 1000000
+        "#,
+        )
+        .unwrap();
+        let backend = Backend::new("test".to_string(), backend_config).unwrap();
+        let storage = LuaStorage::new(backend);
+
+        lua.globals()
+            .set("Response", lua.create_proxy::<LuaResponse>()?)?;
+
+        lua.load(chunk! {
+            // Try to get non-existent response
+            local resp = $storage:get_response({"abc"})
+            assert(resp1 == nil)
+
+            // Store response and fetch it back
+            resp = Response.new({
+                status = 201,
+                headers = {
+                    hello = "world",
+                },
+                body = "test response 1",
+            })
+            local err = $storage:store_response({
+                key = {"abc"},
+                response = resp,
+                surrogate_keys = {"skey1", "skey2"},
+                ttl = 10,
+            })
+            assert(err == nil)
+            resp = $storage:get_response({"abc"})
+            assert(resp.status == 201)
+            assert(resp:header("hello") == "world")
+            assert(resp.body:data() == "test response 1")
+
+            // Delete response
+            $storage:delete_responses({surrogate_keys = {"skey2"}})
+            resp = $storage:get_response({"abc"})
+            assert(resp == nil)
+        })
+        .exec_async()
+        .await
+        .unwrap();
+
+        Ok(())
+    }
 }
