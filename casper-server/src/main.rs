c000c9e6673d67d69a12c494ce3928c370076090
Alex Orlenko <aorlenko@yelp.com>
2023-03-02 11:17:52 +0000

    Use TCP server with http service to track number of connections

--- casper-server/src/main.rs
+++ casper-server/src/main.rs
@@ -1,14 +1,18 @@
-use std::net::TcpListener;
 use std::rc::Rc;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
-use anyhow::Context as _;
 use clap::Parser;
 use ntex::http::client::Client as HttpClient;
+use ntex::http::HttpService;
+use ntex::io::Io;
 use ntex::rt::System;
-use ntex::web::{self, App, HttpServer};
-use tracing::{error, info};
+use ntex::server::Server;
+use ntex::service::{apply_fn_factory, Service};
+use ntex::time::Seconds;
+use ntex::util::PoolId;
+use ntex::web::{self, App};
+use tracing::error;
 use tracing_log::LogTracer;
 
 use crate::context::AppContext;
@@ -59,66 +63,74 @@ async fn main_inner(args: Args) -> anyhow::Result<()> {
     // Drop it
     drop(context);
 
-    let addr = &config.main.listen;
-    let listener = TcpListener::bind(addr).with_context(|| format!("Failed to listen {addr}"))?;
-    info!("Listening on http://{}", addr);
+    let addr = config.main.listen.clone();
+    let workers = config.main.workers;
 
     // Get available CPU cores
     let core_ids = Arc::new(Mutex::new(
         core_affinity::get_core_ids().unwrap_or_default(),
     ));
 
-    let config2 = config.clone();
-    HttpServer::new(move || {
-        // Initialize per-worker thread application context
-        let context = AppContext::builder()
-            .with_config(config.clone())
-            .with_storage_backends(storage_backends.clone())
-            .build()
-            .unwrap();
-        let id = context.id;
+    Server::build()
+        .bind("casper", &addr, move |conf| {
+            conf.memory_pool(PoolId::P0);
 
-        if config.main.pin_workers {
-            if let Some(id) = core_ids.lock().unwrap().pop() {
-                core_affinity::set_for_current(id);
+            // Initialize per-worker thread application context
+            let context = AppContext::builder()
+                .with_config(config.clone())
+                .with_storage_backends(storage_backends.clone())
+                .build()
+                .unwrap();
+            let id = context.id;
+
+            if config.main.pin_workers {
+                if let Some(id) = core_ids.lock().unwrap().pop() {
+                    core_affinity::set_for_current(id);
+                }
             }
-        }
 
-        // Construct default HTTP client and attach it to Lua
-        let http_client = HttpClient::build()
-            .disable_redirects()
-            .disable_timeout()
-            .finish();
-        context.lua.set_app_data(http_client);
+            // Construct default HTTP client and attach it to Lua
+            let http_client = HttpClient::build()
+                .disable_redirects()
+                .disable_timeout()
+                .finish();
+            context.lua.set_app_data(http_client);
 
-        // Track Lua used memory every 10 seconds
-        let lua = Rc::clone(&context.lua);
+            // Track Lua used memory every 10 seconds
+            let lua = Rc::clone(&context.lua);
 
-        tokio::task::spawn_local(async move {
-            loop {
-                lua_used_memory_update!(id, lua.used_memory());
-                tokio::time::sleep(Duration::from_secs(10)).await;
-            }
-        });
+            tokio::task::spawn_local(async move {
+                loop {
+                    lua_used_memory_update!(id, lua.used_memory());
+                    tokio::time::sleep(Duration::from_secs(10)).await;
+                }
+            });
 
-        App::new()
-            .state(context)
-            .wrap(middleware::Metrics::new("/metrics".to_string()))
-            .wrap(middleware::Logger::new())
-            // .wrap(ntex::web::middleware::Logger::default())
-            .default_service(web::to(handler::handler))
-    })
-    // TODO: Use alternative way
-    // .on_connect(|_, ext| {
-    //     // Count number of active connections
-    //     struct ConnectionCountGuard(ActiveCounterGuard);
-    //     ext.insert(ConnectionCountGuard(connections_counter_inc!()));
-    // })
-    .listen(listener)?
-    .keep_alive(30)
-    .workers(config2.main.workers)
-    .run()
-    .await?;
+            let app = App::new()
+                .state(context)
+                .wrap(middleware::Metrics::new("/metrics".to_string()))
+                .wrap(middleware::Logger::new())
+                // .wrap(ntex::web::middleware::Logger::default())
+                .default_service(web::to(handler::handler));
+
+            // TODO: AppConfig
+
+            let service = HttpService::build()
+                .keep_alive(30)
+                .client_timeout(Seconds::new(5))
+                .disconnect_timeout(Seconds::new(5))
+                .finish(app);
+
+            apply_fn_factory(service, |io: Io, handler| {
+                // Count number of active connections
+                let _conn_guard = connections_counter_inc!();
+                handler.call(io)
+            })
+        })?
+        .backlog(2048)
+        .workers(workers)
+        .run()
+        .await?;
 
     Ok(())
 }
