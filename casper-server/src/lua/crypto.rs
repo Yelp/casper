54bf53cdfa4de0a71058e85abfeb34a6b8fcef28
Alex Orlenko <aorlenko@yelp.com>
2023-05-15 14:30:03 +0100

    Add blake3 hash to Lua `crypto`

--- casper-server/src/lua/crypto.rs
+++ casper-server/src/lua/crypto.rs
@@ -1,24 +1,38 @@
 use mlua::{ExternalResult, Lua, Result, String as LuaString, Table, Value};
 use ntex::util::Bytes;
-use openssl::hash::{hash, MessageDigest};
+use openssl::hash::MessageDigest;
 
-async fn digest<'lua>(lua: &'lua Lua, input: Value<'lua>, digest: MessageDigest) -> Result<String> {
+async fn hash<'lua>(lua: &'lua Lua, input: Value<'lua>, digest: MessageDigest) -> Result<String> {
     let bytes = match input {
         Value::UserData(ud) => ud.borrow::<Bytes>()?.clone(),
         _ => Bytes::from(lua.unpack::<LuaString>(input)?.as_bytes().to_vec()),
     };
-    let hashsum = ntex::rt::spawn_blocking(move || hash(digest, bytes.as_ref()))
+    let hashsum = ntex::rt::spawn_blocking(move || openssl::hash::hash(digest, bytes.as_ref()))
         .await
         .expect("failed to join thread")
         .into_lua_err()?;
     Ok(hex::encode(hashsum))
 }
 
+pub async fn blake3<'lua>(lua: &'lua Lua, input: Value<'lua>) -> Result<String> {
+    let bytes = match input {
+        Value::UserData(ud) => ud.borrow::<Bytes>()?.clone(),
+        _ => Bytes::from(lua.unpack::<LuaString>(input)?.as_bytes().to_vec()),
+    };
+    let hashsum = ntex::rt::spawn_blocking(move || blake3::hash(bytes.as_ref()));
+    Ok(hex::encode(
+        hashsum.await.expect("failed to join thread").as_bytes(),
+    ))
+}
+
 pub fn create_module(lua: &Lua) -> Result<Table> {
-    lua.create_table_from([(
-        "sha256",
-        lua.create_async_function(|lua, input| digest(lua, input, MessageDigest::sha256()))?,
-    )])
+    lua.create_table_from([
+        (
+            "sha256",
+            lua.create_async_function(|lua, input| hash(lua, input, MessageDigest::sha256()))?,
+        ),
+        ("blake3", lua.create_async_function(blake3)?),
+    ])
 }
 
 #[cfg(test)]
@@ -32,6 +46,7 @@ mod tests {
         let crypto = super::create_module(&lua)?;
         lua.load(chunk! {
             assert($crypto.sha256("hello") == "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824")
+            assert($crypto.blake3("hello") == "ea8f163db38682925e4491c5e58d4bb3506ef8c14eb78a86e908c5624a67200f")
         })
         .exec_async()
         .await?;
