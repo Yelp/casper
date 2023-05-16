cd655118931a5fd52a1a8b5b925d736bd488dcaa
Alex Orlenko <aorlenko@yelp.com>
2023-05-16 22:00:45 +0100

    Add Lua csv module (initial implementation)

---
+++ casper-server/src/lua/csv.rs
@@ -0,0 +1,85 @@
+use std::cell::RefCell;
+use std::io;
+use std::rc::Rc;
+
+use csv::ByteRecord;
+use mlua::{ExternalResult, Lua, Result, String as LuaString, Table};
+use ntex::util::BytesMut;
+
+// TODO: Full implementation of CSV writer and reader
+
+#[derive(Clone)]
+struct BytesMutCell(Rc<RefCell<BytesMut>>);
+
+impl io::Write for BytesMutCell {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.borrow_mut().extend_from_slice(buf);
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+struct CSVWriter(csv::Writer<BytesMutCell>, BytesMutCell);
+
+fn encode_record<'lua>(lua: &'lua Lua, record: Table) -> Result<LuaString<'lua>> {
+    let mut rec = ByteRecord::new();
+    for field in record.raw_sequence_values::<LuaString>() {
+        let field = field?;
+        rec.push_field(field.as_bytes());
+    }
+    if rec.is_empty() {
+        return lua.create_string("");
+    }
+
+    let mut csv_writer = match lua.app_data_mut::<CSVWriter>() {
+        Some(val) => val,
+        None => {
+            let bytes = BytesMutCell(Rc::new(RefCell::new(BytesMut::new())));
+            let csv_writer = CSVWriter(
+                csv::WriterBuilder::new()
+                    .flexible(true)
+                    .has_headers(false)
+                    .from_writer(bytes.clone()),
+                bytes,
+            );
+            lua.set_app_data(csv_writer);
+            lua.app_data_mut::<CSVWriter>().unwrap()
+        }
+    };
+    // Remove all data if the buffer is not empty
+    csv_writer.1 .0.borrow_mut().clear();
+    csv_writer.0.write_byte_record(&rec).into_lua_err()?;
+    csv_writer.0.flush().into_lua_err()?;
+
+    let s = csv_writer.1 .0.borrow_mut().split().freeze();
+    lua.create_string(s)
+}
+
+pub fn create_module(lua: &Lua) -> Result<Table> {
+    lua.create_table_from([("encode_record", lua.create_function(encode_record)?)])
+}
+
+#[cfg(test)]
+mod tests {
+    use mlua::{chunk, Lua, Result};
+
+    #[test]
+    fn test_module() -> Result<()> {
+        let lua = Lua::new();
+
+        let csv = super::create_module(&lua)?;
+        lua.load(chunk! {
+            assert($csv.encode_record({"a", "b", "c"}) == "a,b,c\n")
+            assert($csv.encode_record({"a", "b", 3}) == "a,b,3\n")
+            assert($csv.encode_record({"a", "b"}) == "a,b\n")
+            assert($csv.encode_record({}) == "")
+            assert($csv.encode_record({1, 2.5, "ab,c"}) == "1,2.5,\"ab,c\"\n")
+        })
+        .exec()?;
+
+        Ok(())
+    }
+}
