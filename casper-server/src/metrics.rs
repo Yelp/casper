27ff1538f6a17b2c7d89cc68a766cc7915f9d074
Alex Orlenko <aorlenko@yelp.com>
2023-08-10 12:39:51 +0100

    Update histogram boundaries

--- casper-server/src/metrics.rs
+++ casper-server/src/metrics.rs
@@ -13,7 +13,7 @@ static METRICS: OnceLock<OpenTelemetryMetrics> = OnceLock::new();
 
 // Histogram boundaries
 static BOUNDARIES: &[f64] = &[
-    0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 7.5, 10.0,
+    0.001, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0,
 ];
 
 pub fn init(config: &Config) {
