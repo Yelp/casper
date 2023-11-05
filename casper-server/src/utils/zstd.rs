7dcff80c098573c3c551a4739afe20b62d1adf9e
Alex Orlenko <aorlenko@yelp.com>
2023-11-05 17:59:52 +0000

    Refactor ZstdDecoder + add tests

--- casper-server/src/utils/zstd.rs
+++ casper-server/src/utils/zstd.rs
@@ -5,7 +5,7 @@ use std::task::{Context, Poll};
 use futures::{ready, stream::Stream};
 use ntex::util::{Buf, Bytes, BytesMut};
 use pin_project_lite::pin_project;
-use zstd::stream::raw::Operation;
+use zstd::stream::raw::{Decoder, Operation, OutBuffer};
 
 pub async fn compress_with_zstd<B>(data: B, level: i32) -> Result<Bytes, IoError>
 where
@@ -21,34 +21,37 @@ pub fn decompress_with_zstd(data: &[u8]) -> Result<Bytes, IoError> {
     zstd::stream::decode_all(data).map(Bytes::from)
 }
 
-enum State {
-    Reading,
-    Writing,
-    Flushing,
-    Done,
-}
-
 pin_project! {
     pub struct ZstdDecoder<S> {
         #[pin]
         stream: S,
-        decoder: zstd::stream::raw::Decoder<'static>,
+        decoder: Decoder<'static>,
         state: State,
         input: Bytes,
         output: BytesMut,
     }
 }
 
-const OUTPUT_BUFFER_SIZE: usize = 8192; // 8KB
+enum State {
+    Reading,
+    Decoding,
+    Flushing,
+    Done,
+}
+
+// This size equals to ZSTD_BLOCKSIZE_MAX
+const OUTPUT_BUFFER_SIZE: usize = 131072; // 128KB
 
 impl<S> ZstdDecoder<S> {
     pub fn new(stream: S) -> Self {
+        let mut output = BytesMut::with_capacity(OUTPUT_BUFFER_SIZE);
+        output.resize(OUTPUT_BUFFER_SIZE, 0);
         Self {
             stream,
-            decoder: zstd::stream::raw::Decoder::new().expect("Unable to create zstd decoder"),
+            decoder: Decoder::new().expect("Unable to create zstd decoder"),
             state: State::Reading,
             input: Bytes::new(),
-            output: BytesMut::new(),
+            output,
         }
     }
 }
@@ -61,71 +64,81 @@ where
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let mut this = self.project();
-
-        let result = (|| loop {
-            let mut temp_buffer = [0u8; OUTPUT_BUFFER_SIZE];
-
-            *this.state = match this.state {
+        loop {
+            match *this.state {
                 State::Reading => {
                     if let Some(chunk) = ready!(this.stream.as_mut().poll_next(cx)) {
                         *this.input = chunk?;
-                        State::Writing
+                        *this.state = State::Decoding;
                     } else {
-                        State::Flushing
+                        *this.state = State::Flushing;
                     }
                 }
 
-                State::Writing => {
+                State::Decoding => {
                     if this.input.is_empty() {
-                        State::Reading
-                    } else {
-                        let status = this
-                            .decoder
-                            .run_on_buffers(&*this.input, &mut temp_buffer)?;
-                        this.input.advance(status.bytes_read);
-                        this.output
-                            .extend_from_slice(&temp_buffer[0..status.bytes_written]);
-                        if status.remaining == 0 {
-                            State::Flushing
-                        } else {
-                            State::Writing
-                        }
+                        *this.state = State::Reading;
+                        continue;
+                    }
+
+                    let status = this.decoder.run_on_buffers(this.input, this.output)?;
+                    this.input.advance(status.bytes_read);
+                    if status.bytes_written > 0 {
+                        let chunk = Bytes::copy_from_slice(&this.output[..status.bytes_written]);
+                        break Poll::Ready(Some(Ok(chunk)));
                     }
                 }
 
                 State::Flushing => {
-                    let mut temp_output_buffer =
-                        zstd::stream::raw::OutBuffer::around(&mut temp_buffer);
-                    let bytes_left = this.decoder.flush(&mut temp_output_buffer)?;
-                    this.output.extend_from_slice(temp_output_buffer.as_slice());
+                    let mut temp_buffer = OutBuffer::around(this.output.as_mut());
+                    let bytes_left = this.decoder.flush(&mut temp_buffer)?;
                     if bytes_left > 0 {
-                        State::Flushing
+                        *this.state = State::Flushing;
                     } else {
-                        State::Done
+                        *this.state = State::Done;
+                    }
+                    if temp_buffer.pos() > 0 {
+                        let chunk = Bytes::copy_from_slice(temp_buffer.as_slice());
+                        break Poll::Ready(Some(Ok(chunk)));
                     }
                 }
 
                 State::Done => {
-                    return Poll::Ready(None);
-                }
-            };
-        })();
-
-        match result {
-            Poll::Ready(Some(Ok(_))) => unreachable!(),
-            Poll::Ready(Some(Err(_))) => {
-                *this.state = State::Done;
-                result
-            }
-            Poll::Ready(None) | Poll::Pending => {
-                if this.output.is_empty() {
-                    result
-                } else {
-                    let result = Poll::Ready(Some(Ok(this.output.split().freeze())));
-                    *this.output = BytesMut::new();
-                    result
+                    break Poll::Ready(None);
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::stream::{self, TryStreamExt};
+    use rand::distributions::{Alphanumeric, DistString};
+
+    #[ntex::test]
+    async fn test_compress_decompress() {
+        let data = Bytes::from_static(b"Hello, world!");
+        let compressed = compress_with_zstd(data.clone(), 0).await.unwrap();
+        assert_ne!(data, compressed);
+        let decompressed = decompress_with_zstd(&compressed).unwrap();
+        assert_eq!(data, decompressed);
+    }
+
+    #[ntex::test]
+    async fn test_decompress_stream() {
+        let raw_data = Alphanumeric.sample_string(&mut rand::thread_rng(), OUTPUT_BUFFER_SIZE * 3);
+        let data = Bytes::from(raw_data);
+        let compressed = compress_with_zstd(data.clone(), 0).await.unwrap();
+        let stream = stream::iter(
+            compressed
+                .chunks(16 * 1024)
+                .map(|chunk| Ok(Bytes::copy_from_slice(chunk))),
+        );
+        let decoder = ZstdDecoder::new(stream);
+        let decoded_chunks = decoder.try_collect::<Vec<Bytes>>().await.unwrap();
+        assert!(decoded_chunks.len() > 1);
+        assert_eq!(decoded_chunks.concat(), data);
     }
 }
