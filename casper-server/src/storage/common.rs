768b61681f76c924dac519718c4f1ca2e555cad8
Alex Orlenko <aorlenko@yelp.com>
2023-11-28 15:08:26 +0000

    Update ntex to 0.7.12 with native http headers serialization. Remove custom serde implementation for ntex::http::header::HeaderMap.

--- casper-server/src/storage/common.rs
+++ casper-server/src/storage/common.rs
@@ -1,14 +1,13 @@
 use ntex::http::header::HeaderMap;
-
-use crate::http::serde as http_serde;
+use serde::{Deserialize, Serialize};
 
 pub fn encode_headers(headers: &HeaderMap) -> Result<Vec<u8>, flexbuffers::SerializationError> {
     let mut serializer = flexbuffers::FlexbufferSerializer::new();
-    http_serde::header_map::serialize(headers, &mut serializer)?;
+    headers.serialize(&mut serializer)?;
     Ok(serializer.take_buffer())
 }
 
 pub fn decode_headers(data: &[u8]) -> Result<HeaderMap, flexbuffers::DeserializationError> {
     let deserializer = flexbuffers::Reader::get_root(data)?;
-    http_serde::header_map::deserialize(deserializer)
+    HeaderMap::deserialize(deserializer)
 }
