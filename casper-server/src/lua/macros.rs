61a2d136f0c40308483d469bbd584686dcf631d7
Alex Orlenko <aorlenko@yelp.com>
2024-03-19 13:51:20 +0000

    Update `lua_try` macro to return underlying error causes as well

--- casper-server/src/lua/macros.rs
+++ casper-server/src/lua/macros.rs
@@ -2,7 +2,7 @@ macro_rules! lua_try {
     ($result:expr) => {
         match $result {
             Ok(ok) => ok,
-            Err(err) => return Ok(Err(err.to_string())),
+            Err(err) => return Ok(Err(format!("{err:#}"))),
         }
     };
 }
