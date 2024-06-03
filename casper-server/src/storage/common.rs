060ab69aa6488bb5276e2aa53758563b1fa0b847
Alex Orlenko <aorlenko@yelp.com>
2024-06-03 15:16:46 +0100

    Remove deprecated flags from Redis storage module: - `EX_COMPRESSED` - `HEADERS_V2`

--- casper-server/src/storage/common.rs
+++ casper-server/src/storage/common.rs
@@ -1,29 +1,13 @@
 use ntex::http::header::HeaderMap;
 use serde::{Deserialize, Serialize};
 
-use crate::http::serde as http_serde;
-
-pub fn encode_headers(
-    headers: &HeaderMap,
-    v2: bool,
-) -> Result<Vec<u8>, flexbuffers::SerializationError> {
+pub fn encode_headers(headers: &HeaderMap) -> Result<Vec<u8>, flexbuffers::SerializationError> {
     let mut serializer = flexbuffers::FlexbufferSerializer::new();
-    if v2 {
-        headers.serialize(&mut serializer)?;
-    } else {
-        http_serde::header_map::serialize(headers, &mut serializer)?;
-    }
+    headers.serialize(&mut serializer)?;
     Ok(serializer.take_buffer())
 }
 
-pub fn decode_headers(
-    data: &[u8],
-    v2: bool,
-) -> Result<HeaderMap, flexbuffers::DeserializationError> {
+pub fn decode_headers(data: &[u8]) -> Result<HeaderMap, flexbuffers::DeserializationError> {
     let deserializer = flexbuffers::Reader::get_root(data)?;
-    if v2 {
-        HeaderMap::deserialize(deserializer)
-    } else {
-        http_serde::header_map::deserialize(deserializer)
-    }
+    HeaderMap::deserialize(deserializer)
 }
